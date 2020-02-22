a4fc1bc975b48882a330cfca37acffa25152066a
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2020-02-22 15:15:24 +0100

    extended: virtualized-router-scripts: fixes
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- recipes-extended/virtualized-router-scripts/files/vms.sh
+++ recipes-extended/virtualized-router-scripts/files/vms.sh
@@ -1,4 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
+function errorCheck {
+    ERROR_CODE="${?}"
+    if [ "${ERROR_CODE}" -ne 0  ]; then
+        echo "${1} : ${ERROR_CODE}"
+        exit 1
+    fi
+}
+
+# Start the 2.4GHz WiFi Access Point
+echo "Starting 2.4GHz WiFi Access Point..."
+systemctl start hostapd
+errorCheck "Failed to start Access Point"
+echo "Access Point started"
+
+
+# Unpack the OPNsense image if required
+if [ ! -f $OPNSENSE_IMG ]; then
+  echo "Could not find OPNsense image..."
+  echo "Trying to unpack..."
+
+  if [ ! -f $OPNSENSE_BZ2 ]; then
+    echo "Could not find OPNsense image archive"
+    exit 1
+  else
+    echo "Unpacking OPNsense image archive..."
+    pv $OPNSENSE_BZ2 | bunzip2 -dk > $OPNSENSE_IMG
+    errorCheck "Failed to unpack $OPNSENSE_BZ2"
+  fi
+fi
+
+
+# Pass 1st Ethernet as WAN for OPNsense
 xl pci-assignable-add 01:00.0
+errorCheck "Failed pass PCI device"
+
 xl create opnsense.cfg
+errorCheck "Failed to create OPNsense VM"
--- recipes-extended/virtualized-router-scripts/virtualized-router-scripts.bb
+++ recipes-extended/virtualized-router-scripts/virtualized-router-scripts.bb
@@ -10,12 +10,12 @@ SRC_URI = " \
 
 S = "${WORKDIR}"
 
+RDEPENDS_${PN} = "opnsense pv hostapd"
+
 do_install() {
-    install -d ${D}${ROOT_HOME}/vrs
+    install -d ${D}${ROOT_HOME}
     install -m  0755 ${S}/vms.sh ${D}${ROOT_HOME}
-    install -m  0644 ${S}/opnsense.cfg ${D}${ROOT_HOME}/vrs
+    install -m  0644 ${S}/opnsense.cfg ${D}${ROOT_HOME}
 }
 
-FILES_${PN} += "${ROOT_HOME}/vrs/ ${ROOT_HOME}/vms.sh"
-
-RDEPENDS_${PN} += "bash"
+FILES_${PN} += "${ROOT_HOME}"
